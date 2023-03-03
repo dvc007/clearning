a425b68a6a25b907c589764fea2d0396855e4c2c
dvclearncode <dvchau2k@gmail.com>
2023-03-03 23:14:07 +0700

    update

--- src/Pages/HomePage/ListBackEnd.js
+++ src/Pages/HomePage/ListBackEnd.js
@@ -83,7 +83,7 @@ export default function ListBackEnd() {
                   }}
                   className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xs px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
-                  Dang Ky
+                  Đăng ký
                 </button>
               </div>
             </div>
